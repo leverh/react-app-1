db8b56a4b68398f67c841241983d37428bd58cdb
leverh <leverh@gmail.com>
2023-08-16 07:34:29 +0000

    Add contexts folder and currentusercontext file. refactor code

--- src/pages/auth/SignInForm.js
+++ src/pages/auth/SignInForm.js
@@ -11,7 +11,7 @@ import Container from "react-bootstrap/Container";
 
 import { Link, useHistory } from "react-router-dom";
 
-import styles from "../../styles/SignInForm.module.css";
+import styles from "../../styles/SignInUpForm.module.css";
 import btnStyles from "../../styles/Button.module.css";
 import appStyles from "../../App.module.css";
 import { useSetCurrentUser } from "../../contexts/CurrentUserContext";
