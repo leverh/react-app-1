8875540c4e7afc94f876c910f159e86d5fa6d8d6
leverh <leverh@gmail.com>
2023-08-16 08:35:05 +0000

    Add post form. Add image preview functionality

--- src/pages/auth/SignUpForm.js
+++ src/pages/auth/SignUpForm.js
@@ -1,10 +1,8 @@
 import React, { useState } from "react";
 import { Link, useHistory } from "react-router-dom";
-
 import styles from "../../styles/SignInUpForm.module.css";
 import btnStyles from "../../styles/Button.module.css";
 import appStyles from "../../App.module.css";
-
 import {
   Form,
   Button,
